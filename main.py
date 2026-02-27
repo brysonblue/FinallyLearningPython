13b5d0c81a4b92c27a47b5e993a6538057a07d23
brysonblue <Brysonlanham@gmail.com>
2026-02-27 08:42:26 -0500

    Added more variables

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Variable = A container for a value (string, integer, float, boolean)
 #            A variable behaves as if it was the value it contains
 
-# Strings
+# Strings, strings are text
 first_name = "Bro"
 food = "pizza"
 email = "Bro123@fake.com"
@@ -10,7 +10,22 @@ print(f"Hello {first_name}")
 print(f"You like {food}")
 print(f"Your email is: {email}")
 
-#Integers
+# Integers, integers are whole numbers
 age = 27
+quantity = 3
+num_of_students = 30
 
 print(f"You are {age} years old")
+print(f"You have {quantity} classes")
+print(f"Your class has {num_of_students} students")
+
+# Float, floats are decimal numbers
+price = 10.99
+gpa = 4.00
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your GPA is {gpa}")
+print(f"You ran {distance} Kilometers")
+
+# Boolean, boolean are true/false, usually 1 for true, 0 for false
