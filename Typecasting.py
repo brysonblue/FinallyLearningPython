84313d937539cbe09520b40890a4923da9186d76
brysonblue <Brysonlanham@gmail.com>
2026-02-27 09:07:06 -0500

    Added a typecasting file to add typecasting.

---
+++ Typecasting.py
@@ -0,0 +1,33 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "Bro Code" # String
+age = 27 # Integer
+gpa = 4.00 # Float
+is_student = True # Boolean
+
+""" 
+type(name)
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+gpa = int(gpa)
+
+print(gpa)
+
+age = float(age)
+print(age)
+
+age = str(age)
+
+print(type(age))
+
+age += "1"
+
+name = bool(name)
+
+print(name)
+
+"""
