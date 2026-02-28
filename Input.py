b0ba9e83c4305b81ef2fd489f8e7017be1625ce6
brysonblue <Brysonlanham@gmail.com>
2026-02-28 09:48:54 -0500

    Added inputs

--- Input.py
+++ Input.py
@@ -1,8 +1,12 @@
 # input() = A function that prompts the user to enter data
 #           Returns the entered data as a string
+# Strings cannot be used with arithmatic expressions
+# Needs to be typecasted to integers or floats
 
 name = input("What is your name?: ")
 age = input("How old are you?: ")
+
+# Or can put age = int(input("How old are you?: "))
 
 age = int(age)
 age = age + 1
