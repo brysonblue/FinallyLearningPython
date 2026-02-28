f3fc961f6ca38cf836fe95767c792a9169396f70
brysonblue <Brysonlanham@gmail.com>
2026-02-28 09:46:48 -0500

    Added inputs

--- Input.py
+++ Input.py
@@ -2,5 +2,12 @@
 #           Returns the entered data as a string
 
 name = input("What is your name?: ")
+age = input("How old are you?: ")
+
+age = int(age)
+age = age + 1
 
 print(f"Hello {name}!")
+print("HAPPY BIRTHDAY!")
+print(f"You are {age} years old.")
+
